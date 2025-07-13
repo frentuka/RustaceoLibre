af00d7305f31f01fc409eed59a54e0c70d86176a
ValentinoKvolek <valenkvolek@gmail.com>
2025-07-13 13:23:30 -0300

    cambios en el Inserte de usuarios  + 100% coverage en producto.rs gg ez

--- structs/producto.rs
+++ structs/producto.rs
@@ -1,5 +1,6 @@
 use ink::{prelude::string::String, primitives::AccountId};
 
+
 use crate::rustaceo_libre::RustaceoLibre;
 
 //
@@ -46,6 +47,7 @@ impl Producto {
         }
     }
 }
+
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[ink::scale_derive(Encode, Decode, TypeInfo)]
@@ -102,4 +104,98 @@ impl RustaceoLibre {
 
         self.productos.get(&id_producto).cloned()
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+        use super::*;
+        use crate::structs::usuario::Rol;
+        use crate::structs::producto::{CategoriaProducto, Producto};
+    #[ink::test]
+    fn registrar_producto_works() {
+        let accounts = ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+        let vendedor = accounts.alice;
+        
+        // Crear contrato
+        let mut contrato = RustaceoLibre::new();
+
+        // Simular llamado como Alice
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(vendedor);
+
+        // Registrar a Alice como Vendedor
+        let rol = Rol::Vendedor(Default::default());
+        assert_eq!(contrato.registrar_usuario(rol), Ok(()));
+
+        // Registrar producto
+        let nombre: String = "Mate".into();
+        let descripcion : String= "Mate de calabaza forrado en cuero".into();
+        let categoria = CategoriaProducto::Cat1;
+        let stock_inicial = 10;
+        
+        let result = contrato.registrar_producto(nombre.clone(), descripcion.clone(), categoria.clone(), stock_inicial);
+        assert!(result.is_ok());
+
+        let id_producto = result.unwrap();
+
+        // Verificar que el producto se haya guardado
+        let producto = contrato.ver_producto(id_producto);
+        assert_eq!(
+            producto,
+            Some(Producto {
+                nombre,
+                descripcion,
+                categoria,
+            })
+        );
+
+        // Verificar que el stock esté en el usuario
+        let usuario = contrato.usuarios.get(vendedor).unwrap();
+        let stock = usuario.obtener_stock_producto(&id_producto);
+        assert_eq!(stock, Some(stock_inicial));
+    }
+
+    #[ink::test]
+    fn registrar_producto_falla_usuario_no_registrado() {
+        let mut contrato = RustaceoLibre::default();
+        let caller = AccountId::from([0x1; 32]);
+
+        let nombre = "Mate".into();
+        let descripcion = "De madera".into();
+        let categoria = CategoriaProducto::Otros;
+        let stock_inicial = 10;
+
+        let result = contrato._registrar_producto(caller, nombre, descripcion, categoria, stock_inicial);
+        
+        assert_eq!(result, Err(ErrorRegistrarProducto::UsuarioNoRegistrado));
+    }
+    #[ink::test]
+    fn registrar_producto_falla_usuario_no_es_vendedor() {
+        let mut contrato = RustaceoLibre::default();
+        let caller = AccountId::from([0x2; 32]);
+
+        // registrar usuario como comprador
+        let rol = Rol::Comprador(Default::default());
+        contrato._registrar_usuario(caller, rol).expect("Debe registrarse");
+
+        let nombre = "Mate".into();
+        let descripcion = "De madera".into();
+        let categoria = CategoriaProducto::Otros;
+        let stock_inicial = 10;
+
+        let result = contrato._registrar_producto(caller, nombre, descripcion, categoria, stock_inicial);
+        
+        assert_eq!(result, Err(ErrorRegistrarProducto::NoEsVendedor));
+    }
+    #[ink::test]
+    fn ver_producto_falla_usuario_no_registrado() {
+    let contrato = RustaceoLibre::default();
+    let caller = AccountId::from([0x4; 32]);
+    let id_producto = 1;
+
+    let resultado = contrato._ver_producto(caller, id_producto);
+
+    assert_eq!(resultado, None);
+}
+
 }
